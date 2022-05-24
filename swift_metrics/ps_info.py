8018cc9ac6c998425956670e3265b510acb6a82f
Tim Burke <tim.burke@gmail.com>
2022-05-24 15:00:42 -0700

    Deal with times == 0

--- swift_metrics/ps_info.py
+++ swift_metrics/ps_info.py
@@ -3,22 +3,24 @@ import subprocess
 
 def merge_proc_info(items, prev_proc_infos):
     pids = {x['pid'] for x in items}
-    cmd = ['ps', '-o', 'pid,rss,vsize,etimes,times,command']
+    cmd = ['ps', '-o', 'pid,pcpu,rss,vsize,etimes,times,command']
     for pid in pids:
         cmd.extend(['-p', str(pid)])
     proc_infos = {}
     for line in subprocess.check_output(
             cmd, encoding='utf-8').strip().split('\n')[1:]:
-        pid, rss, vsize, etimes, times, cmdline = line.split(None, 5)
+        pid, pcpu, rss, vsize, etimes, times, cmdline = line.split(None, 6)
         pid = int(pid)
+        prev_proc_info = prev_proc_infos and prev_proc_infos.get(pid)
         if 'python' in cmdline:
             cmdline = cmdline.partition(' ')[2]
         cmd, _, args = cmdline.partition(' ')
         cmd = os.path.basename(cmd)
         proc_infos[pid] = {
-            'pcpu': float(etimes)/int(times) if not prev_proc_infos else
-                    (float(etimes) - prev_proc_infos['etimes']) /
-                    (int(times) - prev_proc_infos['times']),
+            'pcpu': float(pcpu) if not int(times) else
+                    float(etimes)/int(times) if not prev_proc_info else
+                    (float(etimes) - prev_proc_info['etimes']) /
+                    (int(times) - prev_proc_info['times']),
             'rss': int(rss) * 1024,
             'vsize': int(vsize) * 1024,
             'etimes': int(etimes),
