2b31b509f837113d81728da67dbbd6f74e6e42a5
Tim Burke <tim.burke@gmail.com>
2022-06-14 14:59:07 -0700

    df stats: better handle unmounted drives

--- swift_metrics/df_stats.py
+++ swift_metrics/df_stats.py
@@ -6,18 +6,18 @@ srv_node = pathlib.Path('/srv/node')
 df_headers = ('device', 'total', 'used', 'free', '%used', 'mount')
 
 def df_stats():
-    mounts = [srv_node / x for x in os.listdir(srv_node)]
+    mounts = tuple(srv_node / x for x in os.listdir(srv_node))
     lines = [
         line.split()
         for line in subprocess.run(
-            ['df', '-B', '1'] + mounts,
+            ('df', '-B', '1') + mounts,
             capture_output=True,
             check=True,
             encoding='utf-8',
         ).stdout.split('\n')
-        if line
+        if any(str(m) in line for m in mounts)
     ]
     return [
         {k: v for k, v in zip(df_headers, line)}
-        for line in lines[1:]
+        for line in lines
     ]
